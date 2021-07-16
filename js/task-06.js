4abeccbfd8fcbbcd39136f3d8a4bc5ef05271284
AndrewRiabets <Inferion5@gmail.com>
2021-07-16 21:53:40 +0300

    удалил черновые записи

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,7 @@
 const inputEl = document.querySelector('#validation-input');
-console.dir(inputEl)
-
-// inputEl.addEventListener('change', (event) => {
-//     inputEl.getAttribute('data-length') ?
-//         alert(inputEl.getElementById('validation-input').innerHTML);
-//         inputEl.getElementById('validation-input').id = 'validation-input.valid'
-//         // inputEl.classList.add('validation-input.valid')
-//        :  inputEl.classList.add('validation-input.invalid')
-// } )
 
 inputEl.addEventListener('change', (event) => {
     Number(inputEl.getAttribute('data-length')) === event.target.value.length ?
     inputEl.classList.add('valid')
     : inputEl.classList.add('invalid')
-
 })
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,6 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
-console.dir(textEl)
 inputEl.addEventListener('input', (event) => {
     textEl.style.fontSize = event.currentTarget.value + 'px'
 
